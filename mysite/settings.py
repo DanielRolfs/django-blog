676818864f37cb7c473f7ef0ba7a6b9ff52efc34
DanielRolfs <danielrolfs@icloud.com>
2022-10-05 10:44:24 +0200

    HTML der Site geändert.

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8bcd=vzi6r-jy#b#3_lerrlp7azfvphh$xxh=m2akf=!mb!4ck'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
